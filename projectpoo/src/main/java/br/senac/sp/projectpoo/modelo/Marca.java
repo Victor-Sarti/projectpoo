134210cb4ad0910381d970928c7f770b13beedbe
Victor-sarti <sartivictor24@gmail.com>
2024-10-18 10:12:32 -0300

    add label and buttons

--- projectpoo/src/main/java/br/senac/sp/projectpoo/modelo/Marca.java
+++ projectpoo/src/main/java/br/senac/sp/projectpoo/modelo/Marca.java
@@ -1,5 +1,32 @@
 package br.senac.sp.projectpoo.modelo;
 
 public class Marca {
+	private int id;
+	private String marca;
+	private byte[] logo;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public byte[] getLogo() {
+		return logo;
+	}
+
+	public void setLogo(byte[] logo) {
+		this.logo = logo;
+	}
 
 }
--- projectpoo/src/main/java/br/senac/sp/projectpoo/view/frameMarca.java
+++ projectpoo/src/main/java/br/senac/sp/projectpoo/view/frameMarca.java
@@ -5,11 +5,22 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class frameMarca extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField tfId;
+	private JTextField tfNome;
+	private JTextField textField_2;
+	private JTextField lbLogo;
 
 	/**
 	 * Launch the application.
@@ -31,12 +42,63 @@ public class frameMarca extends JFrame {
 	 * Create the frame.
 	 */
 	public frameMarca() {
+		setTitle("Cadastro de Marcas");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("ID: ");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel.setBounds(10, 12, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNome = new JLabel("NOME:");
+		lblNome.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNome.setBounds(10, 50, 46, 14);
+		contentPane.add(lblNome);
+		
+		tfId = new JTextField();
+		tfId.setEditable(false);
+		tfId.setBounds(63, 11, 137, 20);
+		contentPane.add(tfId);
+		tfId.setColumns(10);
+		
+		tfNome = new JTextField();
+		tfNome.setColumns(10);
+		tfNome.setBounds(63, 49, 249, 20);
+		contentPane.add(tfNome);
+		
+		JButton btnSalvar = new JButton("Salvar");
+		btnSalvar.setMnemonic('s');
+		btnSalvar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnSalvar.setBounds(25, 91, 89, 23);
+		contentPane.add(btnSalvar);
+		
+		JButton btnExcluir = new JButton("Excluir");
+		btnExcluir.setMnemonic('e');
+		btnExcluir.setBounds(124, 91, 89, 23);
+		contentPane.add(btnExcluir);
+		
+		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.setBounds(223, 91, 89, 23);
+		contentPane.add(btnLimpar);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(10, 145, 401, 105);
+		contentPane.add(textField_2);
+		
+		lbLogo = new JTextField();
+		lbLogo.setBackground(Color.LIGHT_GRAY);
+		lbLogo.setBounds(322, 11, 89, 89);
+		lbLogo.setOpaque(true);
+		contentPane.add(lbLogo);
 	}
-
 }
