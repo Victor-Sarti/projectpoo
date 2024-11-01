b436747c40a0ee673f63a3032e59608b28289cae
Victor-sarti <sartivictor24@gmail.com>
2024-11-01 11:27:19 -0300

    hibernat

--- projectpoo/src/main/java/br/senac/sp/projectpoo/modelo/Marca.java
+++ projectpoo/src/main/java/br/senac/sp/projectpoo/modelo/Marca.java
@@ -1,7 +1,17 @@
 package br.senac.sp.projectpoo.modelo;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Marca {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@Column ( name = "nome", columnDefinition = "varchar(100)", nullable = false)
 	private String nome;
 	private byte[] logo;
 
